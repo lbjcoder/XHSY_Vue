be0dde7b915234e2b5ca37aa80c5d0d404d3946b
lbjcoder <lbjcoder@outlook.com>
2020-06-13 13:23:10 +0800

    1

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,17 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <el-container>
+          <el-header>Header</el-header>
+      <el-main>
+          <div id="nav">
+          <router-link to="/">Home</router-link> |
+          <router-link to="/about">About</router-link>
+        </div>
+      <router-view/>
+      </el-main>
+      <el-footer>Footer</el-footer>
+    </el-container>
+   
   </div>
 </template>
 
@@ -29,4 +36,27 @@
 #nav a.router-link-exact-active {
   color: #42b983;
 }
+</style>
+<style>
+  .el-header, .el-footer {
+    background-color: #B3C0D1;
+    color: #333;
+    text-align: center;
+    line-height: 60px;
+  }
+  
+ 
+  .el-main {
+    background-color: #E9EEF3;
+    color: #333;
+    text-align: center;
+    line-height: 160px;
+  }
+  
+  body > .el-container {
+    width: 1200px;
+    margin-bottom: 40px;
+  }
+  
+  
 </style>
